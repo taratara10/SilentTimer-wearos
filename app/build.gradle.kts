328c681c006f6f93d7fe172a155e749eb6ec3f6d
FRONTIER\shuhe <shuhei.knhs@gmail.com>
2023-02-12 18:23:16 +0900

    gradleのkts化して整理

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+plugins {
+    kotlin("android")
+    kotlin("kapt")
+    id("com.android.application")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.kabos.silenttimer"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.kabos.silenttimer"
+        minSdk = 30
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+//            isMinifyEnabled = true
+//            isShrinkResources = true 
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    val composeBom = "2022.12.00"
+    val wear_compose_version = "1.0.0"
+    val horologist_version = "0.1.5"
+    val wear_tiles_version = "1.1.0"
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("com.google.android.gms:play-services-wearable:18.0.0")
+    implementation("androidx.percentlayout:percentlayout:1.0.0")
+    implementation(platform("androidx.compose:compose-bom:$composeBom"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.wear.compose:compose-material:$wear_compose_version")
+    implementation("androidx.wear.compose:compose-foundation:$wear_compose_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.wear.tiles:tiles:$wear_tiles_version")
+    implementation("androidx.wear.tiles:tiles-material:$wear_tiles_version")
+    implementation("com.google.android.horologist:horologist-compose-tools:$horologist_version")
+    implementation("com.google.android.horologist:horologist-tiles:$horologist_version")
+    implementation("androidx.wear.watchface:watchface-complications-data-source-ktx:1.1.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    kapt("com.google.dagger:hilt-compiler:2.44.2")
+}
